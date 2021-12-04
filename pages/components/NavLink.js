6d96721975ee5e588b68aa656c223b2b246d7fae
zark28 <adamsabdulrazak28@gmail.com>
2021-12-04 13:41:15 +0000

    api and logic update2

--- pages/components/NavLink.js
+++ pages/components/NavLink.js
@@ -3,15 +3,19 @@ import { useRouter } from "next/router";
 import React from "react";
 import tw from "tailwind-styled-components/dist/tailwind";
 
-const NavLink = ({ href, children }) => {
+const NavLink = (props) => {
   const router = useRouter();
 
-  let className = children.props.className || "";
-  if (router.pathname === href) {
+  let className = props.children.props.className || "";
+  if (router.pathname === props.href) {
     className = `${className} selected shadow-xl ease-in-out underline text-white rounded-t bg-yellow`;
   }
 
-  return <Link href={href}>{React.cloneElement(children, { className })}</Link>;
+  return (
+    <Link href={props.href}>
+      {React.cloneElement(props.children, { className })}
+    </Link>
+  );
 };
 
 export default NavLink;
